e54e199bdebc90e6646780383c1c82ab042840e6
Pavel Vlasov <freakycue@gmail.com>
2016-10-05 13:06:49 -0400

    empty state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,10 @@
 import React, {Component} from 'react';
 
-import SearchBar from './search_bar';
-import Image from './image';
-
-import {Container} from 'bluecore-ui-kit/lib/layout/Container/Container';
-import {Row} from 'bluecore-ui-kit/lib/layout/Row/Row';
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      status: 'Waiting for input...'
-    };
-  }
-
-  onChange = (searchTerm) => {
-    this.setState({
-      status: 'Waiting for input...',
-      searchTerm
-    });
-  }
-
-  onSubmit = () =>
-    this.setState({
-      status: 'Loading image...',
-      imageUrl: this.state.searchTerm
-    })
-
-  onLoad = () =>
-    this.setState({
-      status: 'Image loaded'
-    })
-
-  onError = () =>
-    this.setState({status: 'Error!!!'})
-
   render() {
-    const {
-      searchTerm,
-      imageUrl,
-      status
-    } = this.state;
 
     return (
-      <Container>
-        <Row>
-          <SearchBar
-            searchTerm={searchTerm}
-            onChange={this.onChange}
-            onSubmit={this.onSubmit} />
-        </Row>
-        <Row>
-          <Image
-            imageUrl={imageUrl}
-            onLoad={this.onLoad}
-            onError={this.onError} />
-        </Row>
-        <Row>
-          Status: {status}
-        </Row>
-      </Container>
+      <h1>Hello World!</h1>
     );
   }
 }
